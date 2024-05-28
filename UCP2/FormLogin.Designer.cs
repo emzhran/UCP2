b61f622365a2b6c54bce5c8c314a1d42e1aa62df
cahyadie <cahyadiilham12@gmail.com>
2024-05-28 14:17:32 +0700

    form

--- UCP2/FormLogin.Designer.cs
+++ UCP2/FormLogin.Designer.cs
@@ -51,7 +51,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.Color.White;
             this.label1.Font = new System.Drawing.Font("Futura BdCn BT", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(364, 71);
+            this.label1.Location = new System.Drawing.Point(441, 79);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 25);
             this.label1.TabIndex = 1;
